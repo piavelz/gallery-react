c2218551083a9b44055acfd3d4db7740f4940ccc
Pia <piag.velasquez@outlook.com>
2024-10-10 17:28:18 -0300

    add dummy components

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+
+import PropTypes from 'prop-types'
+
+const Card = () => {
+  return (
+    <div>Card</div>
+  )
+}
+
+Card.propTypes = {}
+
+export default Card
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+Search.propTypes = {}
+
+export default Search
